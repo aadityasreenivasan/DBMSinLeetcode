b129d9ee495481b9070cc43b857d95046c95dfe6
aadityasreenivasan <104477298+aadityasreenivasan@users.noreply.github.com>
2023-05-18 13:36:59 +0530

    Update Customer Placing the Largest Number of Orders.sql

--- Customer Placing the Largest Number of Orders.sql
+++ Customer Placing the Largest Number of Orders.sql
@@ -1,3 +1,3 @@
-select customer_number from Orders group by customer_number order by count(customer_number)
-Desc limit 1
-;
+select customer_number from Orders 
+group by customer_number order by count(customer_number)
+Desc limit 1;
