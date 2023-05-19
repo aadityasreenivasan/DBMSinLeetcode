dd6dadcbd64735e0c1e4782d2164cd6805a6152c
aadityasreenivasan <104477298+aadityasreenivasan@users.noreply.github.com>
2023-05-19 22:33:56 +0530

    Update Joining3orMoreTables.sql

--- Joining3orMoreTables.sql
+++ Joining3orMoreTables.sql
@@ -19,3 +19,22 @@ insert into marks values(1030, 4, 88, 'pass');
 insert into marks values(1035, 9,  13, 'fail');
 insert into marks values(1040, 5,  16, 'fail');
 insert into marks values(1050, 10, 53, 'pass');
+
+create table details(address_city varchar(20), email_ID varchar(20), school_id int, accomplishments varchar(50));
+
+insert into details values('Banglore',  'jsingh@geeks.com', 
+                                1020, 'ACM ICPC selected');
+insert into details values('Hyderabad', 'praveen@geeks.com', 
+                                1030, 'Geek of the month');
+insert into details values('Delhi',     'rithvik@geeks.com', 
+                                    1012, 'IOI finalist');
+insert into details values('Chennai',   'om@geeks.com', 
+                                 1111, 'Geek of the year');
+insert into details values('Banglore', ' suraj@geeks.com',
+                                 1008, 'IMO finalist');
+insert into details values('Mumbai',    'sasukeh@geeks.com',
+                                  2211, 'Made a robot');
+insert into details values('Ahmedabad', 'itachi@geeks.com',
+                               1172, 'Code Jam finalist');
+insert into details values('Jaipur',    'kumar@geeks.com',
+                                   1972, 'KVPY finalist');
