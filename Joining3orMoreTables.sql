9c0382b2435d73cd41852a9970b4ac83a6d5e1b2
aadityasreenivasan <104477298+aadityasreenivasan@users.noreply.github.com>
2023-05-19 22:32:30 +0530

    Create Joining3orMoreTables.sql

---
+++ Joining3orMoreTables.sql
@@ -0,0 +1,8 @@
+create table student(s_id int primary key, s_name varchar(20));
+
+insert into student values(1, 'Jack');
+insert into student values(2, 'Rithvik');
+insert into student values(3, 'Jaspreet');
+insert into student values(4, 'Praveen');
+insert into student values(5, 'Bisa');
+insert into student values(6, 'Suraj');
