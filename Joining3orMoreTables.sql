6076c5287012cf468564b0e1bde2c07adfdd02a6
aadityasreenivasan <104477298+aadityasreenivasan@users.noreply.github.com>
2023-05-19 22:33:13 +0530

    Update Joining3orMoreTables.sql

--- Joining3orMoreTables.sql
+++ Joining3orMoreTables.sql
@@ -6,3 +6,16 @@ insert into student values(3, 'Jaspreet');
 insert into student values(4, 'Praveen');
 insert into student values(5, 'Bisa');
 insert into student values(6, 'Suraj');
+
+create table marks(school_id int primary key, s_id int, score int, status varchar(20));
+
+insert into marks values(1004, 1, 23, 'fail');
+insert into marks values(1008, 6, 95, 'pass');
+insert into marks values(1012, 2, 97, 'pass');
+insert into marks values(1016, 7, 67, 'pass'); 
+insert into marks values(1020, 3, 100, 'pass');
+insert into marks values(1025, 8, 73, 'pass');
+insert into marks values(1030, 4, 88, 'pass');
+insert into marks values(1035, 9,  13, 'fail');
+insert into marks values(1040, 5,  16, 'fail');
+insert into marks values(1050, 10, 53, 'pass');
