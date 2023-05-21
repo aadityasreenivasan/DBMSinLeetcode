f3384cece4f0407a293b57ee7bde6f5384cdf66b
aadityasreenivasan <104477298+aadityasreenivasan@users.noreply.github.com>
2023-05-21 14:27:55 +0530

    Update Sales Person.sql

--- Sales Person.sql
+++ Sales Person.sql
@@ -1,3 +1,7 @@
 # Write your MySQL query statement below
 select s.name from salesperson as
 where s.sales_id not in
+(
+    select sales_id from as o left join company as c
+    on o.com_id = c.com_id where c.name = 'RED'
+)
