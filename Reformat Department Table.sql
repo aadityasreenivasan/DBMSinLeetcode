400538e6d23f294326084108e9b49187bfd0866d
aadityasreenivasan <104477298+aadityasreenivasan@users.noreply.github.com>
2023-05-26 08:22:15 +0530

    Create Reformat Department Table.sql

---
+++ Reformat Department Table.sql
@@ -0,0 +1,6 @@
+SELECT Department.id,
+sum(if(month='Jan',revenue,null)) as Jan_Revenue,
+sum(if(month='Feb',revenue,null)) as Feb_Revenue,
+sum(if(month='Mar',revenue,null)) as Mar_Revenue,
+sum(if(month='Apr',revenue,null)) as Apr_Revenue,
+sum(if(month='May',revenue,null)) as May_Revenue,
