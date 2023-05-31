563cf36b59a69ba9c59f99950381f60ef9f8cac9
aadityasreenivasan <104477298+aadityasreenivasan@users.noreply.github.com>
2023-05-31 13:35:39 +0530

    Update Top Travellers.sql

--- Top Travellers.sql
+++ Top Travellers.sql
@@ -1,1 +1,2 @@
 SELECT Users.name, IFNULL(SUM(Rides.distance), 0) as travelled_distance FROM Users LEFT JOIN Rides ON Users.id = Rides.user_id
+GROUP BY Rides.user_id ORDER BY travelled_distance DESC, Users.name ;
