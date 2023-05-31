33d5a197ab17bb0393cd2ec91d0f7849c2be998c
aadityasreenivasan <104477298+aadityasreenivasan@users.noreply.github.com>
2023-05-31 13:38:00 +0530

    Update Top Travellers.sql

--- Top Travellers.sql
+++ Top Travellers.sql
@@ -1,2 +1,22 @@
+/*+---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| id            | int     |
+| name          | varchar |
++---------------+---------+
+id is the primary key for this table.
+name is the name of the user.
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| id            | int     |
+| user_id       | int     |
+| distance      | int     |
++---------------+---------+
+id is the primary key for this table.
+user_id is the id of the user who traveled the distance "distance".
+
+*/
 SELECT Users.name, IFNULL(SUM(Rides.distance), 0) as travelled_distance FROM Users LEFT JOIN Rides ON Users.id = Rides.user_id
 GROUP BY Rides.user_id ORDER BY travelled_distance DESC, Users.name ;
