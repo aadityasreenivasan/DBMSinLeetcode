a0104736e63c58db00d62e71e93bed86dc543755
aadityasreenivasan <104477298+aadityasreenivasan@users.noreply.github.com>
2023-05-31 13:43:37 +0530

    Update Reformat Department Table.sql

--- Reformat Department Table.sql
+++ Reformat Department Table.sql
@@ -9,6 +9,10 @@
 (id, month) is the primary key of this table.
 The table has information about the revenue of each department per month.
 The month has values in ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"].
+
+Write an SQL query to reformat the table such that there is a department id column and a revenue column for each month.
+
+Return the result table in any order.
 */
 SELECT Department.id,
 sum(if(month='Jan',revenue,null)) as Jan_Revenue,
