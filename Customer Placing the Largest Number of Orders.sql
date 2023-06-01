b8a2b097b30e58abefd985fd735d3eb5b8158c9b
aadityasreenivasan <104477298+aadityasreenivasan@users.noreply.github.com>
2023-06-01 09:18:15 +0530

    Update Customer Placing the Largest Number of Orders.sql

--- Customer Placing the Largest Number of Orders.sql
+++ Customer Placing the Largest Number of Orders.sql
@@ -1,3 +1,15 @@
+/*
++-----------------+----------+
+| Column Name     | Type     |
++-----------------+----------+
+| order_number    | int      |
+| customer_number | int      |
++-----------------+----------+
+order_number is the primary key for this table.
+This table contains information about the order ID and the customer ID.
+
+Write an SQL query to find the customer_number for the customer who has placed the largest number of orders.
+*/
 select customer_number from Orders 
 group by customer_number order by count(customer_number)
 Desc limit 1;
