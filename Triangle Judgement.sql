d8ae04f81c4ec2444a3356c339de694d78573521
aadityasreenivasan <104477298+aadityasreenivasan@users.noreply.github.com>
2023-06-01 09:16:19 +0530

    Update Triangle Judgement.sql

--- Triangle Judgement.sql
+++ Triangle Judgement.sql
@@ -1,2 +1,13 @@
+/*
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| x           | int  |
+| y           | int  |
+| z           | int  |
++-------------+------+
+(x, y, z) is the primary key column for this table.
+Each row of this table contains the lengths of three line segments.
+*/
 select  *,If(x+y>z and x+z>y and y+z>x,"Yes","No") 
 as triangle from Triangle;
