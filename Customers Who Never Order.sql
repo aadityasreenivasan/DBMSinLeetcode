7c6585fe58822e09e37f710f6bff0eb577c953ca
aadityasreenivasan <104477298+aadityasreenivasan@users.noreply.github.com>
2023-06-03 09:27:13 +0530

    Update Customers Who Never Order.sql

--- Customers Who Never Order.sql
+++ Customers Who Never Order.sql
@@ -1,2 +1,23 @@
+/*
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| name        | varchar |
++-------------+---------+
+id is the primary key column for this table.
+Each row of this table indicates the ID and name of a customer.
+
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| id          | int  |
+| customerId  | int  |
++-------------+------+
+id is the primary key column for this table.
+customerId is a foreign key of the ID from the Customers table.
+Each row of this table indicates the ID of an order and the ID of the customer who ordered it.
+
+*/
 SELECT Name as Customers from Customers
 WHERE id not in (select customerId from orders);
