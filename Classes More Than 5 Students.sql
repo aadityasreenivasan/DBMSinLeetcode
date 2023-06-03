b32129c69d60183887d3f8fed8ff120dde8809c8
aadityasreenivasan <104477298+aadityasreenivasan@users.noreply.github.com>
2023-06-03 09:33:41 +0530

    Update Classes More Than 5 Students.sql

--- Classes More Than 5 Students.sql
+++ Classes More Than 5 Students.sql
@@ -1,1 +1,11 @@
+/*
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| student     | varchar |
+| class       | varchar |
++-------------+---------+
+(student, class) is the primary key column for this table.
+Each row of this table indicates the name of a student and the class in which they are enrolled.
+*/
 select class from Courses group by class Having count(class)>=5; 
