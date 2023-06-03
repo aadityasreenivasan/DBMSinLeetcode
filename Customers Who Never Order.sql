a72ad157cb4a422048685eec364561373a0e97be
aadityasreenivasan <104477298+aadityasreenivasan@users.noreply.github.com>
2023-06-03 09:27:37 +0530

    Update Customers Who Never Order.sql

--- Customers Who Never Order.sql
+++ Customers Who Never Order.sql
@@ -18,6 +18,9 @@ id is the primary key column for this table.
 customerId is a foreign key of the ID from the Customers table.
 Each row of this table indicates the ID of an order and the ID of the customer who ordered it.
 
+Write an SQL query to report all customers who never order anything.
+
+Return the result table in any order.
 */
 SELECT Name as Customers from Customers
 WHERE id not in (select customerId from orders);
