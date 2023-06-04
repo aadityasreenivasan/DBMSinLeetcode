99a33ad98c353c28ebf06904f7888a9b908fc92b
aadityasreenivasan <104477298+aadityasreenivasan@users.noreply.github.com>
2023-06-04 12:33:57 +0530

    Update CombineTwoTables.sql

--- CombineTwoTables.sql
+++ CombineTwoTables.sql
@@ -24,5 +24,8 @@ Table: Address
 addressId is the primary key column for this table.
 Each row of this table contains information about the city and state of one person with ID = PersonId.
 
+Write an SQL query to report the first name, last name, city, and state of each person in the Person table. If the address of a personId is not present in the Address table, report null instead.
+
+Return the result table in any order.
 */
 SELECT p.firstname,p.lastname,a.city,a.state from person p LEFT  OUTER JOIN address  a on p.PersonId=a.PersonId;
