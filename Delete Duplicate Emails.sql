92eda4c2502d38d3fef60821beebc24d2e5dc0e7
aadityasreenivasan <104477298+aadityasreenivasan@users.noreply.github.com>
2023-06-04 12:17:05 +0530

    Update Delete Duplicate Emails.sql

--- Delete Duplicate Emails.sql
+++ Delete Duplicate Emails.sql
@@ -1,1 +1,12 @@
+/*
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| email       | varchar |
++-------------+---------+
+id is the primary key column for this table.
+Each row of this table contains an email. The emails will not contain uppercase letters.
+
+*/
 Delete p1 From person p1, person p2 where p1.email=p2.email and p1.id>p2.id;
