edccc6867c9cd09e6c2d884db73a67b1f062903c
aadityasreenivasan <104477298+aadityasreenivasan@users.noreply.github.com>
2023-06-04 12:02:00 +0530

    Update Not Boring Movies.sql

--- Not Boring Movies.sql
+++ Not Boring Movies.sql
@@ -1,1 +1,14 @@
+/*
++----------------+----------+
+| Column Name    | Type     |
++----------------+----------+
+| id             | int      |
+| movie          | varchar  |
+| description    | varchar  |
+| rating         | float    |
++----------------+----------+
+id is the primary key for this table.
+Each row contains information about the name of a movie, its genre, and its rating.
+rating is a 2 decimal places float in the range [0, 10]
+*/
 select * from Cinema where mod(id,2)=1 and description!="boring" order by rating desc;
