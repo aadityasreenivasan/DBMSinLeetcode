8e24569c7875ab0085b47652be57fbebcceaa539
aadityasreenivasan <104477298+aadityasreenivasan@users.noreply.github.com>
2023-06-04 12:32:33 +0530

    Update CombineTwoTables.sql

--- CombineTwoTables.sql
+++ CombineTwoTables.sql
@@ -1,1 +1,28 @@
+/*
+Table: Person
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| personId    | int     |
+| lastName    | varchar |
+| firstName   | varchar |
++-------------+---------+
+personId is the primary key column for this table.
+This table contains information about the ID of some persons and their first and last names.
+
+Table: Address
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| addressId   | int     |
+| personId    | int     |
+| city        | varchar |
+| state       | varchar |
++-------------+---------+
+addressId is the primary key column for this table.
+Each row of this table contains information about the city and state of one person with ID = PersonId.
+
+*/
 SELECT p.firstname,p.lastname,a.city,a.state from person p LEFT  OUTER JOIN address  a on p.PersonId=a.PersonId;
