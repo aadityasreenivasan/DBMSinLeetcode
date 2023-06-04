f5adf88e1f678636c75712f7d9f1d6d49e62ea8b
aadityasreenivasan <104477298+aadityasreenivasan@users.noreply.github.com>
2023-06-04 12:23:46 +0530

    Update Employees Earning More Than Their Managers.sql

--- Employees Earning More Than Their Managers.sql
+++ Employees Earning More Than Their Managers.sql
@@ -1,3 +1,15 @@
+/*
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| name        | varchar |
+| salary      | int     |
+| managerId   | int     |
++-------------+---------+
+id is the primary key column for this table.
+Each row of this table indicates the ID of an employee, their name, salary, and the ID of their manager.
+*/
 select a.Name as `Employee`
 from `Employee` as a join `Employee` as b
 on a.ManagerId = b.Id
