da8c44053dbe65e2e60975fab983f0da76f07de4
aadityasreenivasan <104477298+aadityasreenivasan@users.noreply.github.com>
2023-06-07 16:33:11 +0530

    Update Employee Bonus.sql

--- Employee Bonus.sql
+++ Employee Bonus.sql
@@ -12,6 +12,17 @@ Table: Employee
 empId is the primary key column for this table.
 Each row of this table indicates the name and the ID of an employee in addition to their salary and the id of their manager.
 
+Table: Bonus
+
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| empId       | int  |
+| bonus       | int  |
++-------------+------+
+empId is the primary key column for this table.
+empId is a foreign key to empId from the Employee table.
+Each row of this table contains the id of an employee and their respective bonus.
 
 */
 select e.name, b.bonus from employee e left join bonus b on e.empId=b.empId 
