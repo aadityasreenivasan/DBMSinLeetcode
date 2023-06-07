4583ce403f5b7fa16849e96d315213ce927df42a
aadityasreenivasan <104477298+aadityasreenivasan@users.noreply.github.com>
2023-06-07 16:30:28 +0530

    Update DuplicateEmails.sql

--- DuplicateEmails.sql
+++ DuplicateEmails.sql
@@ -9,5 +9,9 @@ Table: Person
 +-------------+---------+
 id is the primary key column for this table.
 Each row of this table contains an email. The emails will not contain uppercase letters.
+
+Write an SQL query to report all the duplicate emails. Note that it's guaranteed that the email field is not NULL.
+
+Return the result table in any order.
 */
 select Email from Person group by email having count(email)>1;
