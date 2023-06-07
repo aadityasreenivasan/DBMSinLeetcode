e3b2721940dc414886a1596a455be02663059699
aadityasreenivasan <104477298+aadityasreenivasan@users.noreply.github.com>
2023-06-07 16:35:05 +0530

    Update Rising Temperature.sql

--- Rising Temperature.sql
+++ Rising Temperature.sql
@@ -1,2 +1,18 @@
+/*
+
+Table: Weather
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| id            | int     |
+| recordDate    | date    |
+| temperature   | int     |
++---------------+---------+
+id is the primary key for this table.
+This table contains information about the temperature on a certain day.
+
+
+*/
 select x.id from weather x, weather y
 where x.temperature>y.temperature and datediff(x.recordDate, y.recordDate)=1;
