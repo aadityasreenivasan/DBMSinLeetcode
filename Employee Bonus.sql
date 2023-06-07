c5088c08b0e3f22ee6136039c2e73b0568a8c6c1
aadityasreenivasan <104477298+aadityasreenivasan@users.noreply.github.com>
2023-06-07 16:32:55 +0530

    Update Employee Bonus.sql

--- Employee Bonus.sql
+++ Employee Bonus.sql
@@ -1,2 +1,18 @@
+/*
+Table: Employee
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| empId       | int     |
+| name        | varchar |
+| supervisor  | int     |
+| salary      | int     |
++-------------+---------+
+empId is the primary key column for this table.
+Each row of this table indicates the name and the ID of an employee in addition to their salary and the id of their manager.
+
+
+*/
 select e.name, b.bonus from employee e left join bonus b on e.empId=b.empId 
 where b.bonus is null or b.bonus<1000;
