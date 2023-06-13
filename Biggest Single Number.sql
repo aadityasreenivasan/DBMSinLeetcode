d46963b3a8b90accad010ae6df540170ba065c11
aadityasreenivasan <104477298+aadityasreenivasan@users.noreply.github.com>
2023-06-13 14:21:50 +0530

    Update Biggest Single Number.sql

--- Biggest Single Number.sql
+++ Biggest Single Number.sql
@@ -1,2 +1,14 @@
+/*
+Table: MyNumbers
+
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| num         | int  |
++-------------+------+
+There is no primary key for this table. It may contain duplicates.
+Each row of this table contains an integer.
+
+*/
 SELECT MAX(num) as num from MyNumbers where num in(
 SELECT num from MyNumbers group by num HAVING count(num)=1);
