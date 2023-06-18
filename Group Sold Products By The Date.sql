6cd600a5203a1cb7961d28defcf61f57e61ea365
aadityasreenivasan <104477298+aadityasreenivasan@users.noreply.github.com>
2023-06-18 12:52:08 +0530

    Update Group Sold Products By The Date.sql

--- Group Sold Products By The Date.sql
+++ Group Sold Products By The Date.sql
@@ -1,3 +1,16 @@
+/*
+SQL Schema
+Table Activities:
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| sell_date   | date    |
+| product     | varchar |
++-------------+---------+
+There is no primary key for this table, it may contain duplicates.
+Each row of this table contains the product name and the date it was sold in a market.
+*/
 select sell_date, count(DISTINCT product) as num_sold,
 GROUP_CONCAT(DISTINCT product order by product ASC separator ',') as products
 FROM Activities GROUP BY sell_date ORDER BY sell_date ASC;
