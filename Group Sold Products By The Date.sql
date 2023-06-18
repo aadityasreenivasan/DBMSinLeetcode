a92906e9867a03c993bda33ef3e1b8d139b6f35f
aadityasreenivasan <104477298+aadityasreenivasan@users.noreply.github.com>
2023-06-18 12:52:27 +0530

    Update Group Sold Products By The Date.sql

--- Group Sold Products By The Date.sql
+++ Group Sold Products By The Date.sql
@@ -10,6 +10,12 @@ Table Activities:
 +-------------+---------+
 There is no primary key for this table, it may contain duplicates.
 Each row of this table contains the product name and the date it was sold in a market.
+
+Write an SQL query to find for each date the number of different products sold and their names.
+
+The sold products names for each date should be sorted lexicographically.
+
+Return the result table ordered by sell_date.
 */
 select sell_date, count(DISTINCT product) as num_sold,
 GROUP_CONCAT(DISTINCT product order by product ASC separator ',') as products
