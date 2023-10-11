dfa1bdfc0826ad7fc24e685b01367ff4d6305a63
aadityasreenivasan <104477298+aadityasreenivasan@users.noreply.github.com>
2023-10-12 00:44:56 +0530

    Update Find Users With Valid E-Mails.sql

--- Find Users With Valid E-Mails.sql
+++ Find Users With Valid E-Mails.sql
@@ -1,4 +1,17 @@
 # Write your MySQL query statement below
+/*
+Table: Users
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| user_id       | int     |
+| name          | varchar |
+| mail          | varchar |
++---------------+---------+
+user_id is the primary key (column with unique values) for this table.
+This table contains information of the users signed up in a website. Some e-mails are invalid.
+*/
 SELECT *
 FROM Users
 WHERE mail REGEXP'^[a-z][a-z0-9_.-]*@leetcode[.]com';
