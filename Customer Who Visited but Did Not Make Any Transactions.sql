90dd222b6cdd197a4a90911271632b8eabc60439
aadityasreenivasan <104477298+aadityasreenivasan@users.noreply.github.com>
2023-10-11 16:28:12 +0530

    Update Customer Who Visited but Did Not Make Any Transactions.sql

--- Customer Who Visited but Did Not Make Any Transactions.sql
+++ Customer Who Visited but Did Not Make Any Transactions.sql
@@ -1,3 +1,17 @@
+/*
+Table: Visits
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| visit_id    | int     |
+| customer_id | int     |
++-------------+---------+
+visit_id is the column with unique values for this table.
+This table contains information about the customers who visited the mall.
+ 
+*/
+
 SELECT customer_id, COUNT(visit_id) AS count_no_trans
 FROM Visits
 WHERE visit_id NOT IN(
